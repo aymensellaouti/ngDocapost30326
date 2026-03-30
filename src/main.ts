6edceca5a98ecf3e56a6d59f671c36bac5e0955c
aymensellaouti <aymen.sellaouti@gmail.com>
2026-03-30 19:06:18 +0100

    add angular profile

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,11 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
+  // main() {
+
+  //   f();// appele h //h appele m
+
+  //   g(); // pas d'appel
+  // }
