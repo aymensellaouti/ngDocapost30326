a9e0889649d6962a234c08c20b47e9e28454ce9b
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-09 19:05:20 +0100

    start todo

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,6 +12,7 @@ import { CvPage } from "./cv/cv-page/cv-page";
 import { Ngstyle } from "./directives/ngstyle/ngstyle";
 import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
 import { Ngclass } from "./directives/ngclass/ngclass";
+import { TodoComponent } from "./todo/todo/todo.component";
 
 @Component({
   // Le sélécteur css qui identifie notre composant
@@ -19,7 +20,7 @@ import { Ngclass } from "./directives/ngclass/ngclass";
   // <app-root/>
   selector: 'app-root',
   // Ici je définis les dépendances de mon template
-  imports: [TtcComponent, Som, Pere, CvPage, Ngstyle, MiniWordComponent, Ngclass],
+  imports: [TtcComponent, Som, Pere, CvPage, Ngstyle, MiniWordComponent, Ngclass, TodoComponent],
   // Le fichier HTML que le composant gére
   templateUrl: './app.html',
   // C'est le CSS de CE COMPOSANT
