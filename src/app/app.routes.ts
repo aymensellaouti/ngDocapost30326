94f48894ee4fc734913b2cc8ea22de0f75c1d5fa
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-21 19:30:23 +0100

    mise en place du lazyloading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,36 @@
 import { Routes } from '@angular/router';
-import { First } from './component/first/first';
-import { Color } from './component/color/color';
+
+
 import { CvPage } from './cv/cv-page/cv-page';
-import { TodoComponent } from './todo/todo/todo.component';
-import { MiniWordComponent } from './directives/mini-word/mini-word.component';
-import { Second } from './component/second/second';
-import { DetailsCvComponent } from './cv/details-cv/details-cv.component';
-import { NF404 } from './components/nf404/nf404';
-import { Login } from './auth/login/login';
-import { AddCv } from './cv/add-cv/add-cv';
+
+
+
+
+
+
+
 import { authGuard } from './auth/guards/auth-guard';
 import { detailsCvResolver } from './cv/resolvers/details-cv-resolver';
 
 export const routes: Routes = [
-  { path: '', component: First },
-  { path: 'color', component: Color },
-  { path: 'login', component: Login },
-  { path: 'cv', component: CvPage },
-  { path: 'cv/add', component: AddCv, canActivate: [authGuard] },
+  { path: '', loadComponent: () => import('./component/first/first').then(m => m.First) },
+  { path: 'color', loadComponent: () => import('./component/color/color').then(m => m.Color) },
+  { path: 'login', loadComponent: () => import('./auth/login/login').then(m => m.Login) },
+  { path: 'cv', loadComponent: () => import('./cv/cv-page/cv-page').then(
+    file => file.CvPage
+  ) },
+  { path: 'cv/add', loadComponent: () => import('./cv/add-cv/add-cv').then(m => m.AddCv), canActivate: [authGuard] },
   {
     path: 'cv/:id',
-    component: DetailsCvComponent,
+    loadComponent: () => import('./cv/details-cv/details-cv.component').then(m => m.DetailsCvComponent),
     resolve: {
       // utilises le detailsCvResolver pour résoudre les données
       cv: detailsCvResolver
     }
   },
   // La route de détailsCv
-  { path: 'todo', component: TodoComponent },
-  { path: 'word', component: MiniWordComponent },
-  { path: 'bonjour/:name', component: Second },
-  { path: '**', component: NF404 },
+  { path: 'todo', loadComponent: () => import('./todo/todo/todo.component').then(m => m.TodoComponent) },
+  { path: 'word', loadComponent: () => import('./directives/mini-word/mini-word.component').then(m => m.MiniWordComponent) },
+  { path: 'bonjour/:name', loadComponent: () => import('./component/second/second').then(m => m.Second) },
+  { path: '**', loadComponent: () => import('./components/nf404/nf404').then(m => m.NF404) },
 ];
