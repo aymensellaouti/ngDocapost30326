1a34d09a1f34862635472dbad71021238e1e55a4
aymensellaouti <aymen.sellaouti@gmail.com>
2026-06-08 08:59:25 +0100

    some changes

--- src/app/cv/resolvers/details-cv-resolver.spec.ts
+++ src/app/cv/resolvers/details-cv-resolver.spec.ts
@@ -2,9 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { ResolveFn } from '@angular/router';
 
 import { detailsCvResolver } from './details-cv-resolver';
+import { Cv } from '../model/cv';
 
 describe('detailsCvResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+  const executeResolver: ResolveFn<Cv> = (...resolverParameters) =>
       TestBed.runInInjectionContext(() => detailsCvResolver(...resolverParameters));
 
   beforeEach(() => {
